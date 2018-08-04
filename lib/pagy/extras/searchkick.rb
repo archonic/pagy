32b20cbb8eb2fd2fe81870eae1b15b21de8bac15
Joshua Flark <joshuajmark@gmail.com>
2018-08-03 18:38:24 -0700

    WIP 1

---
+++ lib/pagy/extras/searchkick.rb
@@ -0,0 +1,18 @@
+# See the Pagy documentation: https://ddnexus.github.io/pagy/extras/ TODO
+
+class Pagy
+  # Add specialized backend methods to paginate Searchkick::Results
+  module Backend ; private
+    # Return Pagy object and items
+    def pagy_searchkick(results, vars={})
+      pagy = Pagy.new(pagy_searchkick_get_vars(results, vars))
+      return pagy, array[pagy.offset, pagy.items]
+    end
+
+    def pagy_searchkick_get_vars(results, vars)
+      # Return the merged variables to initialize the Pagy object
+      { count: results.total_count,
+        page:  params[results.options[:page]] }.merge!(vars) #||vars[:page_param]||VARS[:page_param]
+    end
+  end
+end
---
+++ test/pagy/extras/searchkick_test.rb
@@ -0,0 +1,69 @@
+require_relative '../../test_helper'
+require 'pagy/extras/searchkick'
+
+SingleCov.covered!
+
+describe Pagy::Backend do
+
+  let(:backend) { TestController.new }
+
+  describe "#pagy_searchkick" do
+
+    before do
+      @collection = (1..1000).to_a
+    end
+
+    it 'paginates with defaults' do
+      pagy, items = backend.send(:pagy_array, @collection)
+      pagy.must_be_instance_of Pagy
+      pagy.count.must_equal 1000
+      pagy.items.must_equal Pagy::VARS[:items]
+      pagy.page.must_equal backend.params[:page]
+      items.count.must_equal Pagy::VARS[:items]
+      items.must_equal [41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60]
+    end
+
+    it 'paginates with vars' do
+      pagy, items = backend.send(:pagy_array, @collection, page: 2, items: 10, link_extra: 'X')
+      pagy.must_be_instance_of Pagy
+      pagy.count.must_equal 1000
+      pagy.items.must_equal 10
+      pagy.page.must_equal 2
+      pagy.vars[:link_extra].must_equal 'X'
+      items.count.must_equal 10
+      items.must_equal [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    end
+
+  end
+
+  describe "#pagy_searchkick_get_vars" do
+
+    before do
+      @collection = (1..1000).to_a
+    end
+
+    it 'gets defaults' do
+      vars   = {}
+      merged = backend.send :pagy_array_get_vars, @collection, vars
+      merged.keys.must_include :count
+      merged.keys.must_include :page
+      merged[:count].must_equal 1000
+      merged[:page].must_equal 3
+    end
+
+    it 'gets vars' do
+      vars   = {page: 2, items: 10, link_extra: 'X'}
+      merged = backend.send :pagy_array_get_vars, @collection, vars
+      merged.keys.must_include :count
+      merged.keys.must_include :page
+      merged.keys.must_include :items
+      merged.keys.must_include :link_extra
+      merged[:count].must_equal 1000
+      merged[:page].must_equal 2
+      merged[:items].must_equal 10
+      merged[:link_extra].must_equal 'X'
+    end
+
+  end
+
+end
